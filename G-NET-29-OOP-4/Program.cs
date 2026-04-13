7f06b0fc3a580ff100ae5fe4d2521ecd7000ba2b
Gehad Mohamed <gehadmohamedstu@gmail.com>
2026-04-13 14:11:17 +0200

    Q1

--- G-NET-29-OOP-4/Program.cs
+++ G-NET-29-OOP-4/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace G_NET_29_OOP_4
+﻿using System.Numerics;
+
+namespace G_NET_29_OOP_4
 {
+
+    // Part 01 :
+    #region Q1 :
+    //*Static Binding(Compile Time):
+    //    Method overloading
+    //    Method hiding(new keyword)
+    //    Static methods
+    //    Private methods
+    //* Dynamic Binding(Runtime):
+    //    Virtual methods
+    //    Method overriding
+    //    Interface calls
+    #endregion
+
+
+
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+
     }
 }
